bc90464f52ee9a0aff7ba267486034c0470dd032
tienlv@lifull-tech.vn <tienlv@lifull-tech.vn>
2023-01-01 17:54:52 +0700

    Connect to DB, use dotenv package

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import express from 'express';
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
 
+// create instance for app
 const app = express();
+
+// use .env file
+const env = dotenv.config();
+dotenvExpand.expand(env);
+
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.jgt3mof.mongodb.net/Blog?retryWrites=true&w=majority')
+// connect to DB
+mongoose.connect(process.env.MONGO_URL)
     .then(() => app.listen(port, console.log('hello')));
 
 app.use('/api', (req, res, next) => {
